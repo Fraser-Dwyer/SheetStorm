6734bc03ff670ae22e49fc4e27f20566b80da380
Fraser Dwyer <fraserd1413@gmail.com>
2023-11-14 16:54:23 +0000

    Login page now functional

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div>Hello</div>;
+  return <div>Logged in! this is the Home page</div>;
 }
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,19 +1,30 @@
 import "../Styles/Login.css";
 import sheetStormLogo from "../Images/sheetStormLogo4.png";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import { useState } from "react";
 
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
 
   async function handleLogin(e) {
     e.preventDefault();
-    await fetch("http://localhost:4000/login", {
+    const response = await fetch("http://localhost:4000/login", {
       method: "POST",
       body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },
+      credentials: "include",
     });
+    if (response.ok) {
+      setRedirect(true);
+    } else {
+      alert("Invalid login credentials");
+    }
+  }
+
+  if (redirect) {
+    return <Navigate to="/" />;
   }
 
   return (
