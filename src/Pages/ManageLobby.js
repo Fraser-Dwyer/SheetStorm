39c942517a3a8fab1e6a3da97ea28f04c251324e
Fraser Dwyer <fraserd1413@gmail.com>
2023-12-18 15:45:23 +0000

    Manage games css for desktop added

--- src/Pages/ManageLobby.js
+++ src/Pages/ManageLobby.js
@@ -76,92 +76,94 @@ export default function ManageLobby({ baseURL }) {
   }
 
   return (
-    <div className="manageLobbyContainerContainer">
-      <h3>Manage Games</h3>
-      {userCreatedLobbies.length === 0 && (
-        <>
-          <p>You have not yet created any games.</p>
-          <p>
-            To create a game, go to{" "}
-            <a onClick={() => navigate("/create-game")}>Create Game</a>
-          </p>
-        </>
-      )}
-
-      {successMsg && (
-        <div className="successContainerJoinDelete">
-          {successMsg}
-          <div className="closeDiv">
-            <img
-              src={cross}
-              alt="closeImg"
-              onClick={handleCloseClickSuccess}
-            ></img>
-          </div>
-        </div>
-      )}
-
-      {errorMsg && (
-        <div className="errorContainerDelete">
-          {errorMsg}
-          <div className="closeDiv">
-            <img
-              src={cross}
-              alt="closeImg"
-              onClick={handleCloseClickFail}
-            ></img>
-          </div>
-        </div>
-      )}
-
-      {userCreatedLobbies.length > 0 &&
-        userCreatedLobbies.map((lobby) => (
+    <div className="manageLobbyContainerContainerContainer">
+      <div className="manageLobbyContainerContainer">
+        <h3>Manage Games</h3>
+        {userCreatedLobbies.length === 0 && (
           <>
-            <div className="manageLobbyContainer">
-              <div className="flexLobbyContainer">
-                <div className="leftContent">
-                  <div className="infoContainer">
-                    <p>Lobby:</p>
-                    <p>
-                      {lobby.lobbyName.slice(0, 1).toUpperCase()}
-                      {lobby.lobbyName.slice(1)}
-                    </p>
+            <p>You have not yet created any games.</p>
+            <p>
+              To create a game, go to{" "}
+              <a onClick={() => navigate("/create-game")}>Create Game</a>
+            </p>
+          </>
+        )}
+
+        {successMsg && (
+          <div className="successContainerJoinDelete">
+            {successMsg}
+            <div className="closeDiv">
+              <img
+                src={cross}
+                alt="closeImg"
+                onClick={handleCloseClickSuccess}
+              ></img>
+            </div>
+          </div>
+        )}
+
+        {errorMsg && (
+          <div className="errorContainerDelete">
+            {errorMsg}
+            <div className="closeDiv">
+              <img
+                src={cross}
+                alt="closeImg"
+                onClick={handleCloseClickFail}
+              ></img>
+            </div>
+          </div>
+        )}
+
+        {userCreatedLobbies.length > 0 &&
+          userCreatedLobbies.map((lobby) => (
+            <>
+              <div className="manageLobbyContainer">
+                <div className="flexLobbyContainer">
+                  <div className="leftContent">
+                    <div className="infoContainer">
+                      <p>Lobby:</p>
+                      <p>
+                        {lobby.lobbyName.slice(0, 1).toUpperCase()}
+                        {lobby.lobbyName.slice(1)}
+                      </p>
+                    </div>
+                    <div className="infoContainer">
+                      <p>Password:</p>
+                      <p>{lobby.password}</p>
+                    </div>
+                    <div className="infoContainer">
+                      <p>Players:</p>
+                      <p>{lobby.players.length}</p>
+                    </div>
                   </div>
-                  <div className="infoContainer">
-                    <p>Password:</p>
-                    <p>{lobby.password}</p>
-                  </div>
-                  <div className="infoContainer">
-                    <p>Players:</p>
-                    <p>{lobby.players.length}</p>
-                  </div>
-                </div>
-                <div className="rightContent">
-                  <button
-                    onClick={(e) => {
-                      handleConfirmDeleteLobby(e, lobby.lobbyName);
-                    }}
-                  >
-                    Delete
-                  </button>
-                </div>
-              </div>
-              {sure && sure === lobby.lobbyName && (
-                <div className="areYouSure">
-                  <p>Delete {lobby.lobbyName}?</p>
-                  <div>
+                  <div className="rightContent">
                     <button
-                      onClick={(e) => handleDeleteLobby(e, lobby.lobbyName)}
+                      onClick={(e) => {
+                        handleConfirmDeleteLobby(e, lobby.lobbyName);
+                      }}
                     >
                       Delete
                     </button>
-                    <button onClick={() => setSure(null)}>Cancel</button>
                   </div>
                 </div>
-              )}
-            </div>
-          </>
-        ))}
+                {sure && sure === lobby.lobbyName && (
+                  <div className="areYouSure">
+                    <p>Delete {lobby.lobbyName}?</p>
+                    <div>
+                      <button
+                        onClick={(e) => handleDeleteLobby(e, lobby.lobbyName)}
+                      >
+                        Delete
+                      </button>
+                      <button onClick={() => setSure(null)}>Cancel</button>
+                    </div>
+                  </div>
+                )}
+              </div>
+            </>
+          ))}
+      </div>
     </div>
   );
 }
